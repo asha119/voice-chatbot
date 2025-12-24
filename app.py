342490fe7eac25168ec241c7628cdb283d445df0
Asha kiran <k.ashakiran143@gmail.com>
2025-12-24 19:33:44 +0530

    updated chatbot questions

--- app.py
+++ app.py
@@ -18,13 +18,6 @@ def chatbot_response(user_msg):
         return "As per college notification 'GYAN-2k25' is conducting on 26th&27th every student participation is mandatory"
     elif "college timings" in msg:
         return "As per college notification college timings are 9:00AM-4:30PM"
-    elif "sem exams" in msg:
-        return "As per college notification" \
-        "31-12-25---Linear Algebra And calculus" \
-        "02-01-26---Engineering physics" \
-        "05-01-26---Communicative English" \
-        "07-01-26---Basic Civil&Mechanical Engineering" \
-        "09-01-26---Problem Solving and Programming with C"
     elif "this project" in msg:
         return "The Voice-Enabled AI Chatbot is a web-based application developed to assist students by providing instant answers to academic and administrative queries through both text and voice interaction. The system is designed with a professional and mobile-friendly interface, enabling users to ask questions easily and receive responses in real time. It supports features such as multiple responses for the same question, voice input and output, and dynamic interaction for better user experience. The chatbot allows an admin to update or modify questions and answers at any time, ensuring the information remains accurate and up to date. The project is implemented using HTML, CSS, and JavaScript for the frontend, Python with Flask for backend logic, GitHub for version control, and Render for online hosting and accessibility. Overall, the chatbot improves accessibility to information, saves time for students, and provides an efficient digital solution for handling common college-related queries."
     
